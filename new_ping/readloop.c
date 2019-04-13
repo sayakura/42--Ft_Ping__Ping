ebb4acade778a8daf4d99cded0e8eff2aa0e42dd
Kura <linra.kura@gmail.com>
2019-04-13 08:43:53 -0700

    test

--- new_ping/readloop.c
+++ new_ping/readloop.c
@@ -79,6 +79,7 @@ void    readmsg(int b_read)
     if (iphdr->ip_p != IPPROTO_ICMP)
         return ;
     icmp = (struct icmp *)(iphdr + hdrlen);
+    printf("%d\n", hdrlen);
     printf("struct icmp: type: %d, code: %d, id: %d seq: %d\n", icmp->icmp_type, icmp->icmp_code, icmp->icmp_id, icmp->icmp_seq);
     if ((b_read - hdrlen) < 8)
         return ;
