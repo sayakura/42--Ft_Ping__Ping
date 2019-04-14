3f43dbf11b7cbe9a87b7dd89194a44cc76fe8607
Kura <linra.kura@gmail.com>
2019-04-14 16:54:12 -0700

    test

--- helper.c
+++ helper.c
@@ -19,16 +19,14 @@ char* reverse_dns_lookup(char *ip_addr)
     struct sockaddr_in  temp_addr;     
     socklen_t           len; 
     char                buf[100];
-    char                 *ret_buf; 
+    static char         ret_buf[200]; 
   
     temp_addr.sin_family = AF_INET; 
     temp_addr.sin_addr.s_addr = inet_addr(ip_addr); 
     len = sizeof(struct sockaddr_in); 
     if (getnameinfo((struct sockaddr *) &temp_addr, len, buf,  
                     sizeof(buf), NULL, 0, NI_NAMEREQD))  
-        return NULL; 
-    ret_buf = (char*)malloc((strlen(buf) +1)*sizeof(char) ); 
+        return (ip_addr); 
     strcpy(ret_buf, buf); 
-    printf("[%s]\n", ret_buf);
     return ret_buf; 
 } 
