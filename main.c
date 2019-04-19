02f65730ced9c74523d0de97a0b511e339cdae89
Kura <linra.kura@gmail.com>
2019-04-19 09:52:52 -0700

    test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: qpeng <qpeng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 14:51:58 by qpeng             #+#    #+#             */
-/*   Updated: 2019/04/18 17:12:19 by qpeng            ###   ########.fr       */
+/*   Updated: 2019/04/19 09:52:47 by qpeng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,6 +83,7 @@ int     main(int ac, char **av)
     _g.host = av[1];
     _g.min = 0.0000;
     _g.max = 0.0000;
+    _g.times = INT32_MAX;
     signal(SIGALRM, sig_alrm);
     signal(SIGINT, sig_int);
     ping_init(av[1]);
