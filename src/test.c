823b4b95186d9877fb2bdb45517e07b15daca83b
Kura <linra.kura@gmail.com>
2019-04-21 06:18:32 -0700

    test

---
+++ src/test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+struct addrinfo		*host_to_addrinfo(const char *host,\
+						const char *serv, int family, int socktype)
+{
+	struct addrinfo hints;
+	struct addrinfo *res;
+
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_flags = AI_CANONNAME;
+	hints.ai_family = family;
+	hints.ai_socktype = socktype;
+	if (getaddrinfo(host, serv, &hints, &res) != 0)
+	{
+		fprintf(stderr, "ping: %s: Name or service not known\n", host);
+		exit(EXIT_FAILURE);
+	}
+	return (res);
+}
+
+int main()
+{
+    struct addrinfo		*ret;
+
+	ret = host_to_addrinfo("216.58.195.238", NULL, AF_INET, SOCK_STREAM);
+    return 0;
+}
