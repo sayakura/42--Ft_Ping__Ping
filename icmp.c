0b7e129749fb3f25c90b2177989d2f057a27da76
Kura <linra.kura@gmail.com>
2019-04-21 05:15:39 -0700

    test

--- icmp.c
+++ icmp.c
@@ -6,7 +6,7 @@
 /*   By: qpeng <qpeng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 14:51:54 by qpeng             #+#    #+#             */
-/*   Updated: 2019/04/21 04:59:54 by qpeng            ###   ########.fr       */
+/*   Updated: 2019/04/21 05:15:29 by qpeng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,9 +56,9 @@ static void	print_msg(t_info info, bool is_echo)
 	{
 		if (is_echo)
 			if (gl.r_ns_lookup)
-				printf("%d bytes from %s (%s): icmp_seq=%u, %s=%d, time=%.3f ms\
-					\n", info.b_recv, gl.r_host ? gl.r_host : gl.host,\
-					gl.ip, info.seq, info.ver == AF_INET ? "ttl" : "hlim",\
+				printf("%d bytes from %s (%s): icmp_seq=%u, %s=%d, time=%.3f ms"
+					"\n", info.b_recv, gl.r_host ? gl.r_host : gl.host,
+					gl.ip, info.seq, info.ver == AF_INET ? "ttl" : "hlim",
 					info.cnt, info.rrt);
 			else
 				printf("%d bytes from %s: icmp_seq=%u, %s=%d, time=%.3f ms\n",
